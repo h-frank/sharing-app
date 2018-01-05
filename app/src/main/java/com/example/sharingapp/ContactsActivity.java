63d78fbdc2b281407787d89550217030a8d3512d
Bruno G. Tavares <devbruno78@gmail.com>
2018-01-05 16:17:31 -0500

    Add menu

---
+++ app/src/main/java/com/example/sharingapp/ContactsActivity.java
@@ -0,0 +1,13 @@
+package com.example.sharingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+    }
+}
--- app/src/main/java/com/example/sharingapp/MainActivity.java
+++ app/src/main/java/com/example/sharingapp/MainActivity.java
@@ -40,4 +40,23 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, AddItemActivity.class);
         startActivity(intent);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu); //Menu Resource, Menu
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.contacts:
+                Intent intent = new Intent(this, ContactsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
